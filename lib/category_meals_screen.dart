7e5f60363963ea9380c51c99f7bf2f787f5d2825
Juan Dorado <fdorado985@gmail.com>
2020-11-03 15:33:02 -0600

    filter the meals and show them as a text on the cateegory meals screeen

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,19 +1,27 @@
+import 'package:delimeals_flutter/dummy_data.dart';
 import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = '/category-meals';
+
   @override
   Widget build(BuildContext context) {
     final routeArgs =
         ModalRoute.of(context).settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs['title'];
     final categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS
+        .where((meal) => meal.categories.contains(categoryId))
+        .toList();
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
       body: ListView.builder(
-        itemBuilder: (ctx, index) {},
+        itemBuilder: (ctx, index) {
+          return Text(categoryMeals[index].title);
+        },
+        itemCount: categoryMeals.length,
       ),
     );
   }
