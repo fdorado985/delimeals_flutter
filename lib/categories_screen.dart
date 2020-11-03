a87829fe811d164d23228b98ca3162c3bb3bc5f5
Juan Dorado <fdorado985@gmail.com>
2020-11-03 01:16:44 -0600

    Show the categories screen and wrap it on scaffold

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -5,18 +5,23 @@ import 'package:flutter/material.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('DeliMeal'),
       ),
-      children: DUMMY_CATEGORIES
-          .map((category) => CategoryItem(
-                category.title,
-                category.color,
-              ))
-          .toList(),
+      body: GridView(
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          mainAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES
+            .map((category) => CategoryItem(
+                  category.title,
+                  category.color,
+                ))
+            .toList(),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:delimeals_flutter/categories_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: CategoriesScreen(),
     );
   }
 }
