2f44b1aeb2d9254a7228a99e60eb42fd6afb8c20
Juan Dorado <fdorado985@gmail.com>
2020-11-04 20:24:40 -0600

    Add a top tab bar

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -5,25 +5,20 @@ import 'package:flutter/material.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('DeliMeal'),
+    return GridView(
+      padding: EdgeInsets.all(25),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        mainAxisSpacing: 20,
       ),
-      body: GridView(
-        padding: EdgeInsets.all(25),
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 2,
-          mainAxisSpacing: 20,
-        ),
-        children: DUMMY_CATEGORIES
-            .map((category) => CategoryItem(
-                  category.id,
-                  category.title,
-                  category.color,
-                ))
-            .toList(),
-      ),
+      children: DUMMY_CATEGORIES
+          .map((category) => CategoryItem(
+                category.id,
+                category.title,
+                category.color,
+              ))
+          .toList(),
     );
   }
 }
--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -11,6 +11,7 @@ class _TabsScreenState extends State<TabsScreen> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
+      initialIndex: 0,
       length: 2,
       child: Scaffold(
         appBar: AppBar(
